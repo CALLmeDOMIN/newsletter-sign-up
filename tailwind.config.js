f095f19b7e44de57017057bcb68e86ee41286b53
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2023-07-03 23:41:51 +0200

    mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                tomato: "hsl(4, 100%, 67%)",
+                darkSlateGrey: "hsl(234, 29%, 20%)",
+                charcoalGrey: "hsl(235, 18%, 26%)",
+                grey: "hsl(231, 7%, 60%)",
+            },
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
